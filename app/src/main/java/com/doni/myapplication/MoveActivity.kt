a463f281a4dc4115ef6ede7eb161e36fced66a13
doninovandi0711 <1910031802044@sar.ac.id>
2022-03-20 21:08:59 +0700

    first commit

---
+++ app/src/main/java/com/doni/myapplication/MoveActivity.kt
@@ -0,0 +1,11 @@
+package com.doni.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.moveactivity_main)
+    }
+}
